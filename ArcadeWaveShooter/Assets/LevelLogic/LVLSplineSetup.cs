696b7803ac207f4de1370a1d1d3d4d22b791b6c3
Tom <15022636+Thaesch@users.noreply.github.com>
2023-07-16 16:59:42 +0200

    Beispiel Level
    + Changes to LVLSplineSetup

--- ArcadeWaveShooter/Assets/LevelLogic/LVLSplineSetup.cs
+++ ArcadeWaveShooter/Assets/LevelLogic/LVLSplineSetup.cs
@@ -21,11 +21,14 @@ public class LVLSplineSetup : MonoBehaviour
             sA.PlayOnAwake = isFirstSpline;
             sA.Loop = SplineAnimate.LoopMode.Once;
             sA.AnimationMethod = SplineAnimate.Method.Speed;
-            sA.MaxSpeed = 5;
+            if (sA.Container.Spline.TryGetFloatData("Speed", out SplineData<float> speedData))
+                sA.MaxSpeed = speedData.DefaultValue;
+            else
+                sA.MaxSpeed = 2;
             sA.Alignment = SplineAnimate.AlignmentMode.None;
             isFirstSpline = false;
-            if (sA.Container.Spline.TryGetIntData("EasingType", out SplineData<int> data))
-                sA.Easing = data.DefaultValue == 0 ? SplineAnimate.EasingMode.EaseIn : data.DefaultValue == 1 ? SplineAnimate.EasingMode.EaseOut : data.DefaultValue == 2 ? SplineAnimate.EasingMode.EaseInOut : SplineAnimate.EasingMode.None;
+            if (sA.Container.Spline.TryGetIntData("EasingType", out SplineData<int> easingData))
+                sA.Easing = easingData.DefaultValue == 0 ? SplineAnimate.EasingMode.EaseIn : easingData.DefaultValue == 1 ? SplineAnimate.EasingMode.EaseOut : easingData.DefaultValue == 2 ? SplineAnimate.EasingMode.EaseInOut : SplineAnimate.EasingMode.None;
         }
     }
 }
