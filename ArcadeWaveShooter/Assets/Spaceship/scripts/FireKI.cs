c766fa9d6588be66591b7cd3feef6c684f9bcdca
Tom <15022636+Thaesch@users.noreply.github.com>
2023-07-26 22:47:37 +0200

    Kevins KI Krawall
    FIreKI und MovementKI

---
+++ ArcadeWaveShooter/Assets/Spaceship/scripts/FireKI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireKI : MonoBehaviour
+{
+    [SerializeField] private ECSpawner weapon;
+    public float shootInterval = 1.0f; // Time interval between shots
+    private float timeSinceLastShot = 0.0f; // Time since last shot
+
+    private void Update()
+    {
+        timeSinceLastShot += Time.deltaTime; // Increment time since last shot
+
+        if (Input.GetButton("Fire1") && timeSinceLastShot >= shootInterval)
+        {
+            weapon.Spawn(); // Call the Shoot method
+            timeSinceLastShot = 0.0f; // Reset time since last shot
+        }
+    }
+}
---
+++ ArcadeWaveShooter/Assets/Spaceship/scripts/MovementKI.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class MovementKI : MonoBehaviour
+{
+    [SerializeField, Tooltip("Speed of the movement")]
+    private float movementSpeed = 1.0f;
+
+    [SerializeField, Tooltip("Acceleration of the movement")]
+    private float acceleration = 1.0f;
+
+    [SerializeField, Tooltip("Drag of the movement")]
+    private float drag = 1.0f;
+
+    [SerializeField, Tooltip("Maximum speed of the movement")]
+    private float maxSpeed = 5.0f;
+
+    [SerializeField, Tooltip("Upper border")]
+    private float upperBorder = 0;
+
+    [SerializeField, Tooltip("Lower border")]
+    private float lowerBorder = 0;
+
+    [SerializeField, Tooltip("Left border")]
+    private float leftBorder = 0;
+
+    [SerializeField, Tooltip("Right border")]
+    private float rightBorder = 0;
+
+    private float xVelocity = 0;
+    private float yVelocity = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float xChange = Input.GetAxisRaw("Horizontal");
+        float yChange = Input.GetAxisRaw("Vertical");
+
+        float xAccelerationAmount = xChange * acceleration * Time.deltaTime;
+        xVelocity += xAccelerationAmount;
+        xVelocity = Mathf.Clamp(xVelocity, -maxSpeed, maxSpeed);
+
+        float yAccelerationAmount = yChange * acceleration * Time.deltaTime;
+        yVelocity += yAccelerationAmount;
+        yVelocity = Mathf.Clamp(yVelocity, -maxSpeed, maxSpeed);
+
+        float xDragAmount = drag * Time.deltaTime * Mathf.Sign(xVelocity);
+        xVelocity -= xDragAmount;
+
+        float yDragAmount = drag * Time.deltaTime * Mathf.Sign(yVelocity);
+        yVelocity -= yDragAmount;
+
+        float newX = transform.position.x + (movementSpeed * Time.deltaTime * xVelocity);
+        newX = Mathf.Clamp(newX, leftBorder, rightBorder);
+
+        float newY = transform.position.y + (movementSpeed * Time.deltaTime * yVelocity);
+        newY = Mathf.Clamp(newY, lowerBorder, upperBorder);
+
+        if (newX == leftBorder || newX == rightBorder)
+        {
+            xVelocity = 0;
+        }
+
+        if (newY == lowerBorder || newY == upperBorder)
+        {
+            yVelocity = 0;
+        }
+
+        transform.position = new Vector3(newX, newY, 0);
+    }
+}
